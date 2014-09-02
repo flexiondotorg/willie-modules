7d23fc087bef05f5efedbae017e66dec087acbce
Martin Wimpress <code@flexion.org>
2014-09-02 18:16:59 +0100

    Added more phrases and random seed.

--- botsnack.py
+++ botsnack.py
@@ -9,14 +9,21 @@ http://willie.dfbta.net
      
 from __future__ import unicode_literals
 from willie import module
-from random import choice
+import random
      
-RESP = ('ta :)', '=D', 'schweet!', 'thanks :)', 'cheers!', 'Yum!')
+RESP = ('ta :)', '=D', 'schweet!', 'thanks :)', 'cheers!', 'Yum!', 'Nom-nom-nom')
 RESP_NICK = (
     '%(receiver)s, here is a snack from your friendly neighbourhood bot. Enjoy the bits!',
     '%(sender)s, why are you giving away my snacks? %(receiver)s enjoy!',
     '%(sender)s, get your own snacks!',
-    '%(receiver)s, %(sender)s is giving you one of my snacks, enjoy. But I\'m not liking it.')
+    '%(receiver)s, %(sender)s is giving you one of my snacks, enjoy.',
+    '%(sender)s, Hands off my grub! Get your own to give away.',
+    '%(sender)s, is taking my food and giving it to %(receiver)s. I hope it chokes you %(receiver)s!',
+    '%(receiver)s, here is a snack from %(sender)s.',
+)   
+
+def setup(bot):
+    random.seed()
 
 @module.commands('botsnack')
 def botsnack(bot, trigger):
@@ -25,6 +32,6 @@ def botsnack(bot, trigger):
         receiver = trigger.group(2).strip()
         sender = trigger.nick
         nicks = {'sender': sender, 'receiver': receiver}
-        bot.say(choice(RESP_NICK) % nicks)
+        bot.say(random.choice(RESP_NICK) % nicks)
     else:
-        bot.say(choice(RESP))
+        bot.say(random.choice(RESP))
